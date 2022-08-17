65e396b3d96af17d4a8c9ed276b1bda481fab5e0
zvo <zversko@gmail.com>
2022-08-18 00:50:00 +0500

    Change Print HomeWork4/Task3

--- HomeWork4/Task3/Program.cs
+++ HomeWork4/Task3/Program.cs
@@ -6,10 +6,27 @@ int[] RandomArray(int Length)
     int index = 0;
     while(index < array.Length)
     {
-        array[index] = new Random().Next(7,10);
+        array[index] = new Random().Next(0,100);
         index++;
     }
     return array;
+}
+
+int FindTwoMax(int[] a)
+{
+    int count = a.Length;
+    int max1 = 0; 
+    int max2 = 0;
+    for(int i = 0; i < count; i++)
+    {
+        if (a[i] > max1) 
+        {
+            max2 = max1;
+            max1 = a[i];
+        }
+        if (a[i] > max2 && a[i] != max1) max2 = a[i];
+    }
+    return max2;
 }
 
 void PrintArray(int[] a)
@@ -23,24 +40,14 @@ void PrintArray(int[] a)
     }
 }
 
-void PrintMax(int[] a)
+void PrintTwoMax(int max2)
 {
-    int count = a.Length;
-    int max1 = a[0]; 
-    int max2 = a[0];
-    for(int i = 0; i < count; i++)
-    {
-        if (a[i] > max1) 
-        {
-            max2 = max1;
-            max1 = a[i];
-        }
-        if (a[i] > max2 && a[i] != max1) max2 = a[i];
-    }
     Console.WriteLine();
-    Console.Write($"Первый максимум - {max1} ; Второй максимум - {max2}");
+    Console.Write($"Второй максимум - {max2}");
 }
+
+
 
 int[] array = RandomArray(8);
 PrintArray(array);
-PrintMax(array);
+PrintTwoMax(FindTwoMax(array));
